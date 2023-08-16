bfe05f4e1d5a2486339bbaa13c8c80de95aab7fb
JairoCardenas123 <CardenasJairo102011@gmail.com>
2023-08-16 07:24:29 -0500

    avace

--- index.js
+++ index.js
@@ -1,4 +1,24 @@
 const fs = require('fs')
+/* 
+const textIn = fs.readFileSync('./txt/start.txt','utf8')
+console.log(textIn);
 
-const hello = 'hello world';
-console.log(hello);
+const textOut = `Jonas anda en ${textIn}\nCreated in ${Date()} , ` 
+console.log(textOut);
+ */
+/* const hello = 'hello world';
+console.log(hello); */
+
+//Non-block
+
+
+fs.readFile('./txt/start.txt', 'utf8', (err, data1) => {
+    fs.readFile(`./txt/${data1}.txt`, 'utf8', (err,data2)=>{
+        console.log(data2);
+        fs.readFile(`./txt/append.txt`, 'utf8', (err,data3)=>{
+            console.log(data3);
+        })
+    })
+});
+
+console.log('Will read file!');
