16a9d80c57d20cb967f8124f9d3f80b2360ebda5
JairoCardenas123 <CardenasJairo102011@gmail.com>
2023-09-05 07:14:50 -0500

    avance 47:32, Async - Await

--- 3-NODE-ASYNCHRONOUS/index.js
+++ 3-NODE-ASYNCHRONOUS/index.js
@@ -1,26 +1,66 @@
 const fs = require('fs');
 const superagent = require('superagent');
 
-const readFile = file =>{
-    return new Promise((resolve, reject)=>{
-        
-    })
+const readFilePro = file => {
+    return new Promise((resolve, reject) => {
+        fs.readFile(file, (err, data) => {
+            if (err) {
+                reject(new Error('No pude encontrar ese archivo'));
+            } else {
+                resolve(data);
+            }
+        });
+    });
 }
 
-fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
-    console.log(`Breed: ${data}`);
+const writeFilePro = (file, data) => {
+    return new Promise((resolve, reject) => {
+        fs.writeFile(file, data, err => {
+            if (err) {
+                reject(new Error('No se pudo escribir el archivo'));
+            } else {
+                resolve('success');
+            }
+        });
+    });
+}
 
-    superagent
-     .get(`https://dog.ceo/api/breed/${data}/images/random`)
-     .then(res =>{
-        if(err) return console.log(err.message);
-        console.log(res.body.message);          
-        
-        fs.writeFile('dog-img.txt', res.body.message, err =>{
-            if(err) return console.log(err.message);
-            console.log('Random dog image saved to file');
-        })
-     }).catch(err =>{
+const getDogPic = async()=>{
+    try {
+        const data = await readFilePro(`${__dirname}/dog.txt`)
+        console.log(`Raza: ${data}`);
+     
+        const res = await superagent.get(`https://dog.ceo/api/breed/${data}/images/random`)
+        console.log(res.body.message);
+     
+        await writeFilePro('dog-img.txt', res.body.message)
+        console.log('Imagen de un perro aleatorio guardada en el archivo');
+    } catch (error) {
+        console.log(error);
+    }
+
+    return '2:READY'
+  
+
+
+}
+console.log('dassdasdsa');
+const x = getDogPic()
+console.log(x);
+console.log('dassdasdsa');
+
+/* readFilePro(`${__dirname}/dog.txt`)
+    .then(data => {
+        console.log(`Raza: ${data}`);
+        return superagent.get(`https://dog.ceo/api/breed/${data}/images/random`)
+    })
+    .then(res => {
+        console.log(res.body.message);
+        return writeFilePro('dog-img.txt', res.body.message)
+    })
+    .then(() => {
+        console.log('Imagen de un perro aleatorio guardada en el archivo');
+    })
+    .catch(err => {
         console.log(err.message);
-     })
-})
+    }) */
